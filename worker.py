f8163d0b825d0e56ad1e4ec544a52ecdf370af61
Son Dinh Thai <sondinh99999@gmail.com>
2024-02-27 18:18:44 +0700

    edit random error rate of workers

--- worker.py
+++ worker.py
@@ -4,19 +4,25 @@ import numpy as np
 import subprocess
 import random
 
+# receive worker sequence number, F (json) & G (json)
 i = int(sys.argv[1])
 F_json = sys.argv[2]
 F = np.array(json.loads(F_json))
-err = random.randint(1, 10)
+
+err = random.randint(1, 15) # random rate of workers
 
 G_json = sys.argv[3]
 G = np.array(json.loads(G_json))
+
+#create error rate 
 if (err == 1):
     F+=F
-FmulG = np.dot(F, G)
-FmulG_json = json.dumps(FmulG.tolist())
+    
+FmulG = np.dot(F, G) # F mul G
+FmulG_json = json.dumps(FmulG.tolist()) # convert value -> json 
 
-def server(FmulG_json, i):
+# send F mul G to server (from each worker)
+def server(FmulG_json, i): 
     subprocess.run(["python", "server.py", "check", str(i), FmulG_json])
     
 if __name__ == "__main__":
