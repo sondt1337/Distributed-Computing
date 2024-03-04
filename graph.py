fddcc6faee49c2518f99a0029d2e61fcaf2ef9df
Son Dinh Thai <sondinh99999@gmail.com>
2024-03-05 00:45:01 +0700

    comment vi to en

--- graph.py
+++ graph.py
@@ -1,11 +1,9 @@
 import matplotlib.pyplot as plt
 import json
 
-# Chuyển đổi dữ liệu từ tệp graph.txt sang graph.json
+# convert data txt to json
 input_file_path = 'graph.txt'
 output_file_path = 'graph.json'
-
-# Đọc dữ liệu từ file graph.txt và chuyển đổi thành định dạng JSON
 with open(input_file_path, 'r') as input_file:
     lines = input_file.readlines()
 
@@ -13,24 +11,23 @@ data = []
 for line in lines:
     parts = line.split(", ")
     run_number = int(parts[0].split(": ")[0].split(" ")[1])
-    time = float(parts[0].split(": ")[1][:-1])  # Loại bỏ ký tự 's' ở cuối
+    time = float(parts[0].split(": ")[1][:-1])
     incorrect = int(parts[1].split(":")[1])
     correct = int(parts[2].split(":")[1])
     data.append({"run": run_number, "elapsed_time": time, "incorrect": incorrect, "correct": correct})
 
-# Ghi dữ liệu vào file graph.json
 with open(output_file_path, 'w') as output_file:
     json.dump(data, output_file, indent=4)
 
-# Đọc dữ liệu từ file graph.json và tạo biểu đồ
+# create graph
 with open(output_file_path, 'r') as file:
     loaded_data = json.load(file)
 
-# Trích xuất thông tin liên quan
+# insert number of incorrect & elapsed_time
 incorrect_data = [entry["incorrect"] for entry in loaded_data]
 elapsed_time_data = [entry["elapsed_time"] for entry in loaded_data]
 
-# Vẽ biểu đồ
+# Charting
 plt.scatter(incorrect_data, elapsed_time_data, marker='o', color='blue')
 plt.xlabel('Number of Incorrect')
 plt.ylabel('Elapsed Time (seconds)')
